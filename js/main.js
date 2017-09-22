3debe1fec0413f2dc5396dfddedf1d3da99cea69
Sylwester S <uzumagi1@gmail.com>
2017-09-22 19:50:51 +0200

    continued work status: in progress

--- js/main.js
+++ js/main.js
@@ -1,6 +1,21 @@
-$(document).ready(function( ) {
+$(document).ready(function() {
 	$('.slides').slick({
 		autoplay: true,
 		autoplaySpeed: 3000
+	});
+
+// Magnific popout //
+	$('.gallery-image-link').magnificPopup({
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
+		}
 	});
 });
