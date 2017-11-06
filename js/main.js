34e6dc6660291bae0dbf5f47c687c16ec6c0f5de
Sylwester S <uzumagi1@gmail.com>
2017-11-06 16:58:53 +0100

    update project

--- js/main.js
+++ js/main.js
@@ -1,27 +1,3 @@
-$(document).ready(function() {
-	$('.slides').slick({
-		autoplay: true,
-		autoplaySpeed: 3000
-	});
-
-// Magnific popout //
-	$('.gallery-image-link').magnificPopup({
-		type: 'image',
-		tLoading: 'Loading image #%curr%...',
-		mainClass: 'mfp-img-mobile',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.',
-		}
-	});
-});
-
-
-
 var preloaderEl = document.querySelector('#preloader');
 
 setTimeout(function() {
@@ -32,3 +8,40 @@ setTimeout(function() {
 		this.classList.remove('preloader-hiding');																							 
 	});
 }, 1000);
+
+$(document).ready(function() {
+	
+	// Slider 
+	$('.slides').slick({
+		autoplay: true,
+		autoplaySpeed: 3000
+	});
+	
+	// Magnific popup
+	$('.gallery-image-link').magnificPopup({
+		type: 'image',
+		tLoading: 'Loading image #%curr%...',
+		mainClass: 'mfp-img-mobile',
+		gallery: {
+			enabled: true,
+			navigateByImgClick: true,
+			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+		},
+		image: {
+			tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+		}
+	});
+	
+	// Smooth scroll
+	$('.menu-links a').smoothScroll({
+		offset: -100,
+		afterScroll: function() {
+			$(this).closest('.menu-links').find('a').removeClass('active');
+			$(this).addClass('active');
+		}
+	});
+	
+});
+
+
+
